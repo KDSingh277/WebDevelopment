fb78cf1e7ae9635670c229466af7a0e5b175755a
Karan Singh <deepkaran277@gmail.com>
2026-06-22 00:05:03 -0700

    Started compononets

---
+++ ottergram/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import {Heading} from '@chakra-ui/react';
+
+function Header() {
+  return (
+    <header className='header-component'>
+      <Heading as="h1" size="xl">Ottergram</Heading>
+    </header>
+  );
+}
+
+export default Header;
