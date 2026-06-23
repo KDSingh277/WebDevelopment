5231e52169dbdf92e88c1246f14f2b7d27bba834
Karan Singh <deepkaran277@gmail.com>
2026-06-22 23:45:42 -0700

    Completed the Bronze challenge

--- ottergram/src/components/Post.jsx
+++ ottergram/src/components/Post.jsx
@@ -1,12 +1,19 @@
 import { Box, Button } from '@chakra-ui/react'
 
 function Post({image, name}){
-    const handleClick = (otterName) => {
-        return console.log('You clicked on' + otterName)
+    // const handleClick = (otterName) => {
+    //     return console.log('You clicked on' + otterName)
+    // }
+    const mouseOver = (otterName) => {
+        return console.log('Hello, ' + otterName)
+    }
+    const mouseAway = (otterName) => {
+        return console.log('Goodbye, ' + otterName)
     }
     return (
         <Box as="li" className='post-component'>
-            <Button onClick = {() => handleClick(name)} h="auto" p={4} flexDirection="column" colorScheme="teal" variant="solid">
+            <Button onMouseEnter = {() => mouseOver(name)} onMouseLeave={() => mouseAway(name)} 
+            h="auto" p={4} flexDirection="column" colorScheme="teal" variant="solid">
                 <img src={image} alt={name} />
                 <p className='post-name'>{name}</p>
             </Button>
